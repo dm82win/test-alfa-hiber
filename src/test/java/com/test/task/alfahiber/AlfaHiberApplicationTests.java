7285852e8ea839ac5dc7d094f76f919c9d3ebcc5
Dmitry Chekunov <dm82win@yandex.com>
2023-05-14 15:43:43 +0400

    feat: Application started

---
+++ src/test/java/com/test/task/alfahiber/AlfaHiberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.task.alfahiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfaHiberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
